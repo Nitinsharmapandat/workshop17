b82d7cc154c7a1e83139521b7bb2be5ae37b1127
Nitin sharma <nitinsharma75604@gmail.com>
2021-10-21 17:04:04 +0530

    initial commit

---
+++ productData.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+      id: "1",
+      name: "PhotoShop",
+      price: 300,
+    },
+    {
+      id: "2",
+      name: "Webstorm",
+      price: 300,
+    },
+    {
+      id: "3",
+      name: "Figma",
+      price: 300,
+    },
+  ];
+  
+  module.exports = products;
+  
